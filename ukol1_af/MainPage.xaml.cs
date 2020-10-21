f2148b622cac9d81e582bab5d2dc1dadb7fdc23c
bajtekv <bajtekvaclav@gmail.com>
2020-10-21 17:20:59 +0200

    Initial commit

---
+++ ukol1_af/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ukol1_af
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new ViewModel();
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ ukol1_af/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ukol1_af
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        double _vysledekMesicne = 0;
+        double _vysledekRocne = 0;
+        public int VyseUveru { get; set; }
+        public float UrokovaSazba { get; set; }
+        public int DobaSplaceni { get; set; }
+        public double VysledekMesicne
+        {
+            get => _vysledekMesicne;
+            set { _vysledekMesicne = value; OnPropertyChanged(nameof(VysledekMesicne)); }
+        }
+        public double VysledekCelkem
+        {
+            get => _vysledekRocne;
+            set { _vysledekRocne = value; OnPropertyChanged(nameof(VysledekCelkem)); }
+        }
+        public Command Vypocet { get; set; }
+
+        public ViewModel()
+        {
+            Vypocet = new Command(Spocitej);
+        }
+        private void Spocitej()
+        {
+            double v, i;
+            i = (UrokovaSazba / 100) / 12;
+            v = 1 / (1 + i);
+            VysledekMesicne = i * VyseUveru / (1 - Math.Pow(v, DobaSplaceni * 12));
+            VysledekCelkem = VysledekMesicne * DobaSplaceni * 12;
+        }
+    }
+}
